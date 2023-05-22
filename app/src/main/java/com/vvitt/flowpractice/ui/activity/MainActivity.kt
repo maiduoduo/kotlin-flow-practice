2a6452e8974c12b654d9ac9b93764cd495a48407
maiduoduo <maiduoduo0@163.com>
2023-05-22 17:30:15 +0800

    Flow 的综合应用-初始化

---
+++ app/src/main/java/com/vvitt/flowpractice/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vvitt.flowpractice.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vvitt.flowpractice.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
